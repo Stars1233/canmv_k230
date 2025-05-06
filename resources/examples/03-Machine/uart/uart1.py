b45312d33cb96718bf589f2164021a2359364d71
kendryte747 <kendryte747@gmail.com>
2025-05-06 15:52:58 +0800

    Fix examples.

--- resources/examples/03-Machine/uart/uart1.py
+++ resources/examples/03-Machine/uart/uart1.py
@@ -5,19 +5,13 @@ import time
 # 实例化FPIOA
 from machine import FPIOA
 fpioa = FPIOA()
-#pin3 设置为串口1发送管脚
+#设置pin3为串口1发送管脚
 fpioa.set_function(3, fpioa.UART1_TXD)
 #设置pin4为串口1接收管脚
 fpioa.set_function(4, fpioa.UART1_RXD)
-#使能pin3的输入输出功能
-fpioa.set_function(3,set_ie=1,set_oe=1)
-#使能pin3的输入输出功能
-fpioa.set_function(4,set_ie=1,set_oe=1)
-
 
 #fpioa.set_function(5, fpioa.UART2_TXD)
 #fpioa.set_function(6, fpioa.UART2_RXD)
-
 
 #UART: baudrate 115200, 8bits, parity none, one stopbits
 uart = UART(UART.UART1, baudrate=115200, bits=UART.EIGHTBITS, parity=UART.PARITY_NONE, stop=UART.STOPBITS_ONE)
