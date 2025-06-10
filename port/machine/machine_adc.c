20d2aaeb0910f702041730ea3f5e34ef09d4de17
kendryte747 <kendryte747@gmail.com>
2025-06-10 14:53:14 +0800

    Update machine.ADC

--- port/machine/machine_adc.c
+++ port/machine/machine_adc.c
@@ -28,87 +28,89 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include <fcntl.h>
-#include <unistd.h>
-#include <errno.h>
-#include <sys/ioctl.h>
-
-#include "py/runtime.h"
 #include "py/obj.h"
+#include "py/runtime.h"
 
 #include "modmachine.h"
 
-#define ADC_DEVICE_NAME "/dev/adc"
-#define REF_VOL         (1.8)
-#define RESOLUTION      (4096 - 1)
-#define ADC_CHANNEL_MAX 6
+#include "drv_adc.h"
 
 typedef struct {
     mp_obj_base_t base;
-    uint8_t channel;
+
+    int channel;
 } machine_adc_obj_t;
 
-static int adc_fd = -1;
-
-static int adc_read_value(uint8_t channel) {
-    uint32_t value;
-
-    lseek(adc_fd, channel, SEEK_SET);
-    read(adc_fd, &value, sizeof(value));
-
-    return value;
-}
-
-STATIC void machine_adc_print(const mp_print_t *print, mp_obj_t self_in, mp_print_kind_t kind) {
-    machine_adc_obj_t *self = MP_OBJ_TO_PTR(self_in);
+STATIC void machine_adc_print(const mp_print_t* print, mp_obj_t self_in, mp_print_kind_t kind)
+{
+    machine_adc_obj_t* self = MP_OBJ_TO_PTR(self_in);
     mp_printf(print, "ADC: %d", self->channel);
 }
 
-STATIC mp_obj_t machine_adc_make_new(const mp_obj_type_t *type, size_t n_args, size_t n_kw, const mp_obj_t *args) {
+STATIC mp_obj_t machine_adc_make_new(const mp_obj_type_t* type, size_t n_args, size_t n_kw, const mp_obj_t* args)
+{
     mp_arg_check_num(n_args, n_kw, 1, 1, true);
 
     int channel = mp_obj_get_int(args[0]);
-    if (channel < 0 || channel >= ADC_CHANNEL_MAX) {
-        mp_raise_msg_varg(&mp_type_ValueError, MP_ERROR_TEXT("channel is invalid"));
+    if ((0 > channel) || (DRV_ADC_MAX_CHANNEL <= channel)) {
+        mp_raise_msg_varg(&mp_type_ValueError, MP_ERROR_TEXT("invalid adc channel"));
     }
 
-    if (adc_fd < 0) {
-        adc_fd = open(ADC_DEVICE_NAME, O_RDWR);
-        if (adc_fd < 0) {
-            mp_raise_OSError_with_filename(errno, ADC_DEVICE_NAME);
-        }
+    if (0x00 != drv_adc_init()) {
+        mp_raise_msg_varg(&mp_type_RuntimeError, MP_ERROR_TEXT("adc init failed"));
     }
 
-    /* RT_ADC_CMD_ENABLE = 0 */
-    uint32_t chn = channel;
-    if(0x00 != ioctl(adc_fd, 0, chn)) {
-        mp_raise_msg_varg(&mp_type_OSError, "adc enable channel %d failed", channel);
-    }
+    machine_adc_obj_t* self = m_new_obj_with_finaliser(machine_adc_obj_t);
 
-    machine_adc_obj_t *self = mp_obj_malloc(machine_adc_obj_t, &machine_adc_type);
-    self->channel = channel;
+    self->base.type = &machine_adc_type;
+    self->channel   = channel;
 
     return MP_OBJ_FROM_PTR(self);
 }
 
-STATIC mp_obj_t machine_adc_read_u16(mp_obj_t self_in) {
-    machine_adc_obj_t *self = MP_OBJ_TO_PTR(self_in);
-    return mp_obj_new_int_from_uint(adc_read_value(self->channel));
+STATIC mp_obj_t machine_adc_destroy(mp_obj_t self_in)
+{
+    drv_adc_deinit();
+
+    return mp_const_none;
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_1(machine_adc_destroy_obj, machine_adc_destroy);
+
+STATIC mp_obj_t machine_adc_read_u16(mp_obj_t self_in)
+{
+    machine_adc_obj_t* self = MP_OBJ_TO_PTR(self_in);
+
+    uint32_t value = drv_adc_read(self->channel);
+
+    return mp_obj_new_int_from_uint(value & 0xFFFF);
 }
 STATIC MP_DEFINE_CONST_FUN_OBJ_1(machine_adc_read_u16_obj, machine_adc_read_u16);
 
-STATIC mp_obj_t machine_adc_read_uv(mp_obj_t self_in) {
-    machine_adc_obj_t *self = MP_OBJ_TO_PTR(self_in);
-    return mp_obj_new_int_from_uint((uint32_t)(adc_read_value(self->channel) * REF_VOL * 1e6 / RESOLUTION));
+STATIC mp_obj_t machine_adc_read_uv(size_t n, const mp_obj_t* args)
+{
+    machine_adc_obj_t* self = MP_OBJ_TO_PTR(args[0]);
+
+    uint32_t read, ref_uv = DRV_ADC_DEFAULT_REF_UV;
+
+    if (0x02 <= n) {
+        ref_uv = mp_obj_get_int(args[1]);
+    }
+
+    read = drv_adc_read_uv(self->channel, ref_uv);
+
+    return mp_obj_new_int_from_uint(read);
 }
-STATIC MP_DEFINE_CONST_FUN_OBJ_1(machine_adc_read_uv_obj, machine_adc_read_uv);
+STATIC MP_DEFINE_CONST_FUN_OBJ_VAR_BETWEEN(machine_adc_read_uv_obj, 1, 2, machine_adc_read_uv);
 
 STATIC const mp_rom_map_elem_t machine_adc_locals_dict_table[] = {
+    { MP_ROM_QSTR(MP_QSTR___del__), MP_ROM_PTR(&machine_adc_destroy_obj) },
+
     { MP_ROM_QSTR(MP_QSTR_read_u16), MP_ROM_PTR(&machine_adc_read_u16_obj) },
     { MP_ROM_QSTR(MP_QSTR_read_uv), MP_ROM_PTR(&machine_adc_read_uv_obj) },
 };
 STATIC MP_DEFINE_CONST_DICT(machine_adc_locals_dict, machine_adc_locals_dict_table);
 
+/* clang-format off */
 MP_DEFINE_CONST_OBJ_TYPE(
     machine_adc_type,
     MP_QSTR_ADC,
@@ -116,4 +118,5 @@ MP_DEFINE_CONST_OBJ_TYPE(
     make_new, machine_adc_make_new,
     print, machine_adc_print,
     locals_dict, &machine_adc_locals_dict
-    );
+);
+/* clang-format on */
