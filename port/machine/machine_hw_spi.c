0f594fa3ced3370bba3567ac01238f76c680cbe4
zzy <zhengzeying@canaan-creative.com>
2025-06-23 15:52:36 +0800

    use spi hal interface

--- port/machine/machine_hw_spi.c
+++ port/machine/machine_hw_spi.c
@@ -43,114 +43,10 @@
 #include "extmod/machine_spi.h"
 
 #include "modmachine.h"
-
-/**
- * At CPOL=0 the base value of the clock is zero
- *  - For CPHA=0, data are captured on the clock's rising edge (low->high transition)
- *    and data are propagated on a falling edge (high->low clock transition).
- *  - For CPHA=1, data are captured on the clock's falling edge and data are
- *    propagated on a rising edge.
- * At CPOL=1 the base value of the clock is one (inversion of CPOL=0)
- *  - For CPHA=0, data are captured on clock's falling edge and data are propagated
- *    on a rising edge.
- *  - For CPHA=1, data are captured on clock's rising edge and data are propagated
- *    on a falling edge.
- */
-#define RT_SPI_CPHA     (1<<0)                             /* bit[0]:CPHA, clock phase */
-#define RT_SPI_CPOL     (1<<1)                             /* bit[1]:CPOL, clock polarity */
-
-#define RT_SPI_CS_HIGH  (1<<4)                             /* Chipselect active high */
-
-#define RT_SPI_MODE_MASK    (RT_SPI_CPHA | RT_SPI_CPOL | RT_SPI_CS_HIGH)
-
-#define RT_SPI_MODE_0       (0 | 0)                        /* CPOL = 0, CPHA = 0 */
-#define RT_SPI_MODE_1       (0 | RT_SPI_CPHA)              /* CPOL = 0, CPHA = 1 */
-#define RT_SPI_MODE_2       (RT_SPI_CPOL | 0)              /* CPOL = 1, CPHA = 0 */
-#define RT_SPI_MODE_3       (RT_SPI_CPOL | RT_SPI_CPHA)    /* CPOL = 1, CPHA = 1 */
-
-#define RT_SPI_DEV_CTRL_CONFIG      (12 * 0x100 + 0x01)
-#define RT_SPI_DEV_CTRL_RW          (12 * 0x100 + 0x02)
-
-enum {
-    K230_SPI_HARD_CS0 = 0x80 | (1 << 0),
-    K230_SPI_HARD_CS1 = 0x80 | (1 << 1),
-    K230_SPI_HARD_CS2 = 0x80 | (1 << 2),
-    K230_SPI_HARD_CS3 = 0x80 | (1 << 3),
-};
-
-/**
- * SPI configuration structure
- */
-struct rt_spi_configuration
-{
-    uint8_t mode;
-    uint8_t data_width;
-
-    union {
-        struct {
-            uint16_t hard_cs : 8; // hard spi cs configure
-            uint16_t soft_cs : 8; // 0x80 | pin
-        };
-        uint16_t reserved;
-    };
-
-    uint32_t max_hz;
-};
-
-struct rt_qspi_configuration
-{
-    struct rt_spi_configuration parent;
-    /* The size of medium */
-    uint32_t medium_size;
-    /* double data rate mode */
-    uint8_t ddr_mode;
-    /* the data lines max width which QSPI bus supported, such as 1, 2, 4 */
-    uint8_t qspi_dl_width;
-};
-
-/**
- * SPI message structure
- */
-struct rt_spi_message
-{
-    const void *send_buf;
-    void *recv_buf;
-    size_t length;
-    struct rt_spi_message *next;
-
-    unsigned cs_take    : 1;
-    unsigned cs_release : 1;
-};
-
-struct rt_qspi_message
-{
-    struct rt_spi_message parent;
-
-    /* instruction stage */
-    struct {
-        uint32_t content;
-        uint8_t size;
-        uint8_t qspi_lines;
-    } instruction;
-
-    /* address and alternate_bytes stage */
-    struct {
-        uint32_t content;
-        uint8_t size;
-        uint8_t qspi_lines;
-    } address, alternate_bytes;
-
-    /* dummy_cycles stage */
-    uint32_t dummy_cycles;
-
-    /* number of lines in qspi data stage, the other configuration items are in parent */
-    uint8_t qspi_data_lines;
-};
+#include "drv_spi.h"
 
 typedef struct _machine_hw_spi_obj_t {
     mp_obj_base_t base;
-
-    int dev_fd;
 
     uint8_t index;
     uint8_t polarity;
@@ -158,8 +54,7 @@ typedef struct _machine_hw_spi_obj_t {
     uint8_t bits;
     uint8_t data_line;
 
-    uint8_t hard_cs_cfg;
-    uint8_t soft_cs_val;
+    int cs;
 
     enum {
         MACHINE_HW_SPI_STATE_NONE,
@@ -168,37 +63,20 @@ typedef struct _machine_hw_spi_obj_t {
     } state;
 
     uint32_t baudrate;
+    drv_spi_inst_t spi_inst;
 } machine_hw_spi_obj_t;
 
 STATIC machine_hw_spi_obj_t machine_hw_spi_obj[3];
 
 void machine_hw_spi_transfer(mp_obj_base_t *self_in, size_t len, const uint8_t *src, uint8_t *dest) {
     machine_hw_spi_obj_t *self = MP_OBJ_TO_PTR(self_in);
-    struct rt_qspi_message msg;
 
     if (MACHINE_HW_SPI_STATE_DEINIT == self->state) {
         mp_raise_msg(&mp_type_OSError, MP_ERROR_TEXT("transfer on deinitialized SPI"));
         return;
     }
 
-    if(0x00 == len) {
-        return;
-    }
-
-    memset(&msg, 0, sizeof(msg));
-
-    msg.parent.send_buf = src;
-    msg.parent.recv_buf = dest;
-    msg.parent.length = len;
-
-    msg.parent.next = NULL;
-    msg.parent.cs_take = 0;
-    msg.parent.cs_release = 0;
-
-    msg.qspi_data_lines = 1;
-
-    // at now, we use qspi driver, the result is transfer length.
-    if(len != ioctl(self->dev_fd, RT_SPI_DEV_CTRL_RW, &msg)) {
+    if (len != drv_spi_transfer(self->spi_inst, src, dest, len, 0)) {
         mp_printf(&mp_plat_print, "maybe spi transfer failed, %d\n", errno);
     }
 }
@@ -208,13 +86,11 @@ void machine_hw_spi_transfer(mp_obj_base_t *self_in, size_t len, const uint8_t *
 STATIC void machine_hw_spi_print(const mp_print_t *print, mp_obj_t self_in, mp_print_kind_t kind) {
     machine_hw_spi_obj_t *self = MP_OBJ_TO_PTR(self_in);
 
-    const char *cs_type[] = {"hard", "soft"};
-    uint8_t is_soft_cs = (0x00 == self->hard_cs_cfg) ? 1 : 0;
-    uint8_t cs_val = is_soft_cs ? (self->soft_cs_val & 0x7F) : (self->hard_cs_cfg & 0x7F);
+    uint8_t cs_val = self->cs;
 
-    mp_printf(print, "SPI(id=%u, baudrate=%u, polarity=%u, phase=%u, bits=%u, data_line=%u, cs=%u (%s))",
+    mp_printf(print, "SPI(id=%u, baudrate=%u, polarity=%u, phase=%u, bits=%u, data_line=%u, cs=%u)",
         self->index, self->baudrate, self->polarity,
-        self->phase, self->bits, self->data_line, cs_val, cs_type[is_soft_cs]);
+        self->phase, self->bits, self->data_line, cs_val);
 }
 
 STATIC void machine_hw_spi_deinit(mp_obj_base_t *self_in) {
@@ -222,9 +98,7 @@ STATIC void machine_hw_spi_deinit(mp_obj_base_t *self_in) {
 
     if (self->state == MACHINE_HW_SPI_STATE_INIT) {
         self->state = MACHINE_HW_SPI_STATE_DEINIT;
-
-        close(self->dev_fd);
-        self->dev_fd = -1;
+        drv_spi_inst_destroy(&self->spi_inst);
     }
 }
 
@@ -296,28 +170,13 @@ STATIC void machine_hw_spi_init(mp_obj_base_t *self_in, size_t n_args, const mp_
     }
 
     if(-1 != cs) {
-        uint8_t hard_cs_cfg = (int8_t)-1;
-        uint8_t soft_cs_val = (int8_t)-1;
-
-        if(0x80 & cs) {
-            hard_cs_cfg = cs;
-
-            if((K230_SPI_HARD_CS0 > (uint8_t)cs) || (K230_SPI_HARD_CS3 < (uint8_t)cs)) {
-                mp_raise_msg_varg(&mp_type_ValueError, MP_ERROR_TEXT("Invalid spi cs(%d)"), cs);
-            }
-        } else {
-            soft_cs_val = cs | 0x80;
-
-            if((0 > cs) || (63 < cs)) {
-                mp_raise_msg_varg(&mp_type_ValueError, MP_ERROR_TEXT("Invalid spi cs(%d)"), cs);
-            }
+        if((0 > cs) || (63 < cs)) {
+            mp_raise_msg_varg(&mp_type_ValueError, MP_ERROR_TEXT("Invalid spi cs(%d)"), cs);
         }
 
-        if((self->hard_cs_cfg != hard_cs_cfg) || (self->soft_cs_val != soft_cs_val)) {
+        if(self->cs != cs) {
             changed = true;
-
-            self->hard_cs_cfg = hard_cs_cfg;
-            self->soft_cs_val = soft_cs_val;
+            self->cs = cs;
         }
     }
 
@@ -352,25 +211,8 @@ STATIC void machine_hw_spi_init(mp_obj_base_t *self_in, size_t n_args, const mp_
         return;
     }
 
-    char dev_name[32];
-    struct rt_qspi_configuration spi_config;
-
-    snprintf(dev_name, sizeof(dev_name), "/dev/spi%d", self->index);
-
-    if(0 > (self->dev_fd = open(dev_name, O_RDWR))) {
-        mp_raise_msg(&mp_type_RuntimeError, MP_ERROR_TEXT("Open spi device failed"));
-    }
-
-    spi_config.parent.mode = (polarity << 1) | (phase << 0);
-    spi_config.parent.data_width = bits;
-    spi_config.parent.hard_cs = self->hard_cs_cfg;
-    spi_config.parent.soft_cs = self->soft_cs_val;
-    spi_config.parent.max_hz = self->baudrate;
-
-    spi_config.qspi_dl_width = data_line;
-
-    if(0x00 != ioctl(self->dev_fd, RT_SPI_DEV_CTRL_CONFIG, &spi_config)) {
-        mp_raise_msg(&mp_type_RuntimeError, MP_ERROR_TEXT("Set spi device configure failed"));
+    if (0 != drv_spi_inst_create(self->index, 0, (polarity << 1) | (phase << 0), self->baudrate, bits, cs, data_line, &self->spi_inst)) {
+        mp_raise_msg(&mp_type_RuntimeError, MP_ERROR_TEXT("create spi inst failed"));
     }
 
     self->state = MACHINE_HW_SPI_STATE_INIT;
@@ -429,11 +271,6 @@ STATIC const mp_rom_map_elem_t machine_spi_locals_dict_table_k230[] = {
     { MP_ROM_QSTR(MP_QSTR_readinto), MP_ROM_PTR(&mp_machine_spi_readinto_obj) },
     { MP_ROM_QSTR(MP_QSTR_write), MP_ROM_PTR(&mp_machine_spi_write_obj) },
     { MP_ROM_QSTR(MP_QSTR_write_readinto), MP_ROM_PTR(&mp_machine_spi_write_readinto_obj) },
-
-    { MP_ROM_QSTR(MP_QSTR_HARD_CS0), MP_ROM_INT(K230_SPI_HARD_CS0) },
-    { MP_ROM_QSTR(MP_QSTR_HARD_CS1), MP_ROM_INT(K230_SPI_HARD_CS1) },
-    { MP_ROM_QSTR(MP_QSTR_HARD_CS2), MP_ROM_INT(K230_SPI_HARD_CS2) },
-    { MP_ROM_QSTR(MP_QSTR_HARD_CS3), MP_ROM_INT(K230_SPI_HARD_CS3) },
 };
 MP_DEFINE_CONST_DICT(mp_machine_spi_locals_dict_k230, machine_spi_locals_dict_table_k230);
 
