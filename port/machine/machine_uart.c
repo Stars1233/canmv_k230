45847243d12931d89d1abeebaf00d67c27b5bbc0
kendryte747 <kendryte747@gmail.com>
2025-06-27 14:05:22 +0800

    refactor(machine.UART): adopt official API and drv_uart HAL

--- port/machine/machine_uart.c
+++ port/machine/machine_uart.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2023, Canaan Bright Sight Co., Ltd
+/* Copyright (c) 2025, Canaan Bright Sight Co., Ltd
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -27,242 +27,267 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include <fcntl.h>
-#include <unistd.h>
-#include <errno.h>
-#include <sys/ioctl.h>
-
 #include "mpprint.h"
-#include "py/runtime.h"
 #include "py/obj.h"
+#include "py/runtime.h"
 #include "py/stream.h"
 
+#include "drv_uart.h"
 #include "modmachine.h"
-
-#define IOC_SET_BAUDRATE        _IOW('U', 0x40, int)
-#define	IOC_GET_BUFFER_SIZE     _IOW('U', 0x41, int)
 
 typedef struct {
     mp_obj_base_t base;
-    int fd;
-    uint32_t timeout;
-    uint32_t baudrate;
-    uint8_t index;
-    uint8_t bitwidth;
-    uint8_t parity;
-    uint8_t stop;
-    uint8_t status;
+    int           index;
+    int           status;
+    int           timeout;
+
+    int baudrate, bitwidth, parity, stop;
+
+    drv_uart_inst_t* inst;
+
 } machine_uart_obj_t;
 
-struct uart_configure {
-    uint32_t baud_rate;
-    uint32_t data_bits  : 4;
-    uint32_t stop_bits  : 2;
-    uint32_t parity     : 2;
-    uint32_t fifo_lenth : 2;
-    uint32_t auto_flow  : 1;
-    uint32_t reserved   : 21;
-};
-
-// uart0 for console used.
-static bool uart_used[5] = {1, 0, 0, 0, 0};
-
-STATIC void machine_uart_obj_check(machine_uart_obj_t *self) {
-    if (self->status == 0) {
-        mp_raise_msg_varg(&mp_type_OSError, MP_ERROR_TEXT("The UART object has been deleted"));
-    }
+STATIC void machine_uart_print(const mp_print_t* print, mp_obj_t self_in, mp_print_kind_t kind)
+{
+    machine_uart_obj_t* self = MP_OBJ_TO_PTR(self_in);
+    mp_printf(print, "UART(%u, baudrate=%u, bits=%u, parity=%u, stop=%u)", self->index, self->baudrate, self->bitwidth,
+              self->parity, self->stop);
 }
 
-STATIC void machine_uart_print(const mp_print_t *print, mp_obj_t self_in, mp_print_kind_t kind) {
-    machine_uart_obj_t *self = MP_OBJ_TO_PTR(self_in);
-    machine_uart_obj_check(self);
-    mp_printf(print, "UART %u: baudrate=%u, bitwidth=%u, parity=%u, stop=%u", self->index,
-        self->baudrate, self->bitwidth, self->parity, self->stop);
-}
-
-STATIC void machine_uart_init_helper(machine_uart_obj_t *self, size_t n_args, const mp_obj_t *pos_args, mp_map_t *kw_args) {
+STATIC void machine_uart_init_helper(machine_uart_obj_t* self, size_t n_args, const mp_obj_t* pos_args, mp_map_t* kw_args)
+{
     enum { ARG_baudrate, ARG_bits, ARG_parity, ARG_stop, ARG_timeout };
     static const mp_arg_t allowed_args[] = {
-        { MP_QSTR_baudrate, MP_ARG_INT, {.u_int = 115200} },
-        { MP_QSTR_bits, MP_ARG_INT, {.u_int = 8} },
-        { MP_QSTR_parity, MP_ARG_INT, {.u_int = 0} },
-        { MP_QSTR_stop, MP_ARG_INT, {.u_int = 0} },
-        { MP_QSTR_timeout, MP_ARG_KW_ONLY | MP_ARG_INT, {.u_int = -1} },
+        { MP_QSTR_baudrate, MP_ARG_INT, { .u_int = 115200 } },
+        { MP_QSTR_bits, MP_ARG_INT, { .u_int = 8 } },
+        { MP_QSTR_parity, MP_ARG_INT, { .u_int = PARITY_NONE } },
+        { MP_QSTR_stop, MP_ARG_INT, { .u_int = STOP_BITS_1 } },
+        { MP_QSTR_timeout, MP_ARG_KW_ONLY | MP_ARG_INT, { .u_int = 10 } },
     };
     mp_arg_val_t args[MP_ARRAY_SIZE(allowed_args)];
     mp_arg_parse_all(n_args, pos_args, kw_args, MP_ARRAY_SIZE(allowed_args), allowed_args, args);
 
     self->baudrate = args[ARG_baudrate].u_int;
     self->bitwidth = args[ARG_bits].u_int;
-    self->parity = args[ARG_parity].u_int;
-    self->stop = args[ARG_stop].u_int;
+    self->parity   = args[ARG_parity].u_int;
+    self->stop     = args[ARG_stop].u_int;
 
-    // set timeout
     if (args[ARG_timeout].u_int != -1) {
         self->timeout = args[ARG_timeout].u_int;
     }
 
-    struct uart_configure conf;
-    conf.baud_rate = self->baudrate;
-    conf.data_bits = self->bitwidth;
-    conf.parity = self->parity;
-    conf.stop_bits = self->stop;
-    conf.fifo_lenth = 2;
-    conf.auto_flow = 0;
+    struct uart_configure cfg = {
+        .baud_rate = self->baudrate,
+        .data_bits = self->bitwidth,
+        .stop_bits = self->stop,
+        .parity    = self->parity,
+        .bit_order = BIT_ORDER_LSB,
+        .invert    = NRZ_NORMAL,
+        .bufsz     = 0x400, // default
+        .reserved  = 0,
+    };
 
-    ioctl(self->fd, IOC_SET_BAUDRATE, &conf);
+    if (0x00 != drv_uart_set_config(self->inst, &cfg)) {
+        mp_printf(&mp_plat_print, "uart%d set config failed", self->index);
+    }
 }
 
-STATIC mp_obj_t machine_uart_make_new(const mp_obj_type_t *type, size_t n_args, size_t n_kw, const mp_obj_t *args) {
-    mp_arg_check_num(n_args, n_kw, 1, 5, true);
+STATIC mp_obj_t machine_uart_make_new(const mp_obj_type_t* type, size_t n_args, size_t n_kw, const mp_obj_t* args)
+{
+    mp_arg_check_num(n_args, n_kw, 1, MP_OBJ_FUN_ARGS_MAX, true);
 
     int index = mp_obj_get_int(args[0]);
-    if (index < 0 || index > 4) {
-        mp_raise_ValueError(MP_ERROR_TEXT("invalid uart number"));
-    }
-    if (uart_used[index]) {
-        mp_raise_msg_varg(&mp_type_OSError, MP_ERROR_TEXT("UART %u busy"), index);
+    if (index < 0 || index >= KD_HARD_UART_MAX_NUM) {
+        mp_raise_ValueError(MP_ERROR_TEXT("invalid UART index"));
     }
 
-    char dev_name[16] = "/dev/uart0";
-    dev_name[9] = '0' + index;
-    int fd = open(dev_name, O_RDWR);
-    if (fd < 0) {
-        mp_raise_OSError_with_filename(errno, dev_name);
+    drv_uart_inst_t* inst = NULL;
+    if (drv_uart_inst_create(index, &inst) < 0) {
+        mp_raise_msg_varg(&mp_type_OSError, MP_ERROR_TEXT("cannot create UART %u"), index);
     }
 
-    machine_uart_obj_t *self = m_new_obj_with_finaliser(machine_uart_obj_t);
-    self->base.type = &machine_uart_type;
-    self->index = index;
-    self->fd = fd;
-    self->status = 1;
-    self->timeout = 0;
+    machine_uart_obj_t* self = m_new_obj_with_finaliser(machine_uart_obj_t);
+    self->base.type          = &machine_uart_type;
+    self->index              = index;
+    self->inst               = inst;
+    self->status             = 1;
+    self->timeout            = 0;
 
     mp_map_t kw_args;
     mp_map_init_fixed_table(&kw_args, n_kw, args + n_args);
     machine_uart_init_helper(self, n_args - 1, args + 1, &kw_args);
-    self->status = 2;
-    uart_used[index] = 1;
 
+    self->status = 2;
     return MP_OBJ_FROM_PTR(self);
 }
 
-STATIC mp_obj_t machine_uart_deinit(mp_obj_t self_in) {
-    machine_uart_obj_t *self = MP_OBJ_TO_PTR(self_in);
-    if (self->status == 0)
+STATIC mp_obj_t machine_uart_deinit(mp_obj_t self_in)
+{
+    machine_uart_obj_t* self = MP_OBJ_TO_PTR(self_in);
+    if (self->status == 0) {
         return mp_const_none;
-    close(self->fd);
-    if (self->status == 2)
-        uart_used[self->index] = 0;
+    }
+
+    drv_uart_inst_destroy(&self->inst);
     self->status = 0;
 
     return mp_const_none;
 }
 STATIC MP_DEFINE_CONST_FUN_OBJ_1(machine_uart_deinit_obj, machine_uart_deinit);
 
-STATIC mp_obj_t machine_uart_init(mp_uint_t n_args, const mp_obj_t *args, mp_map_t *kw_args) {
-    machine_uart_obj_check(args[0]);
+STATIC mp_obj_t machine_uart_init(mp_uint_t n_args, const mp_obj_t* args, mp_map_t* kw_args)
+{
     machine_uart_init_helper(args[0], n_args - 1, args + 1, kw_args);
     return mp_const_none;
 }
 STATIC MP_DEFINE_CONST_FUN_OBJ_KW(machine_uart_init_obj, 1, machine_uart_init);
 
-STATIC mp_obj_t machine_uart_any(mp_obj_t self_in) {
-    machine_uart_obj_t *self = MP_OBJ_TO_PTR(self_in);
+STATIC mp_obj_t machine_uart_any(mp_obj_t self_in)
+{
+    machine_uart_obj_t* self = MP_OBJ_TO_PTR(self_in);
 
-    size_t buffer_size = 0;
+    int ret = drv_uart_poll(self->inst, 0);
 
-    if(0x00 != ioctl(self->fd, IOC_GET_BUFFER_SIZE, &buffer_size)) {
-        buffer_size = 0;
-        mp_printf(&mp_plat_print, "ERROR: read uart buffer size failed.\n");
-    }
-
-    return MP_OBJ_NEW_SMALL_INT(buffer_size);
+    return MP_OBJ_NEW_SMALL_INT(ret > 0 ? 1 : 0);
 }
 STATIC MP_DEFINE_CONST_FUN_OBJ_1(machine_uart_any_obj, machine_uart_any);
 
-STATIC mp_uint_t machine_uart_read(mp_obj_t self_in, void *buf_in, mp_uint_t size, int *errcode) {
-    machine_uart_obj_t *self = MP_OBJ_TO_PTR(self_in);
-    machine_uart_obj_check(self);
-
-    int bytes_read = 0;
-    mp_uint_t curr_time_ms, time_out_ms = mp_hal_ticks_ms() + self->timeout;
-
-    do {
-        bytes_read += read(self->fd, buf_in + bytes_read, size - bytes_read);
-
-        curr_time_ms = mp_hal_ticks_ms();
-    } while((bytes_read != size) && (curr_time_ms < time_out_ms));
-
-    if(bytes_read <= 0) {
-        *errcode = MP_EAGAIN;
-        return MP_STREAM_ERROR;
-    }
-
-    return bytes_read;
+STATIC mp_obj_t machine_uart_sendbreak(mp_obj_t self_in)
+{
+    machine_uart_obj_t* self = MP_OBJ_TO_PTR(self_in);
+    drv_uart_send_break(self->inst);
+    return mp_const_none;
 }
+STATIC MP_DEFINE_CONST_FUN_OBJ_1(machine_uart_sendbreak_obj, machine_uart_sendbreak);
 
-STATIC mp_uint_t machine_uart_write(mp_obj_t self_in, const void *buf_in, mp_uint_t size, int *errcode) {
-    machine_uart_obj_t *self = MP_OBJ_TO_PTR(self_in);
-    machine_uart_obj_check(self);
-
-    int bytes_written = write(self->fd, buf_in, size);
-
-    if (bytes_written < 0) {
-        *errcode = MP_EAGAIN;
-        return MP_STREAM_ERROR;
-    }
-
-    return bytes_written;
+STATIC mp_obj_t machine_uart_txdone(mp_obj_t self_in)
+{
+    mp_printf(&mp_plat_print, "K230 not support UART.txdone()\n");
+    return mp_const_false;
 }
-
-STATIC mp_uint_t machine_uart_ioctl(mp_obj_t self_in, mp_uint_t request, uintptr_t arg, int *errcode) {
-    mp_uint_t ret;
-
-    *errcode = MP_EINVAL;
-    ret = MP_STREAM_ERROR;
-
-    return ret;
-}
+STATIC MP_DEFINE_CONST_FUN_OBJ_1(machine_uart_txdone_obj, machine_uart_txdone);
 
 STATIC const mp_rom_map_elem_t machine_uart_locals_dict_table[] = {
     { MP_ROM_QSTR(MP_QSTR___del__), MP_ROM_PTR(&machine_uart_deinit_obj) },
+
     { MP_ROM_QSTR(MP_QSTR_init), MP_ROM_PTR(&machine_uart_init_obj) },
     { MP_ROM_QSTR(MP_QSTR_deinit), MP_ROM_PTR(&machine_uart_deinit_obj) },
     { MP_ROM_QSTR(MP_QSTR_any), MP_ROM_PTR(&machine_uart_any_obj) },
-    { MP_ROM_QSTR(MP_QSTR_readline), MP_ROM_PTR(&mp_stream_unbuffered_readline_obj)},
-    { MP_ROM_QSTR(MP_QSTR_readinto), MP_ROM_PTR(&mp_stream_readinto_obj) },
-    { MP_ROM_QSTR(MP_QSTR_write), MP_ROM_PTR(&mp_stream_write_obj) },
     { MP_ROM_QSTR(MP_QSTR_read), MP_ROM_PTR(&mp_stream_read_obj) },
+    { MP_ROM_QSTR(MP_QSTR_readinto), MP_ROM_PTR(&mp_stream_readinto_obj) },
+    { MP_ROM_QSTR(MP_QSTR_readline), MP_ROM_PTR(&mp_stream_unbuffered_readline_obj) },
+    { MP_ROM_QSTR(MP_QSTR_write), MP_ROM_PTR(&mp_stream_write_obj) },
+    { MP_ROM_QSTR(MP_QSTR_sendbreak), MP_ROM_PTR(&machine_uart_sendbreak_obj) },
+    { MP_ROM_QSTR(MP_QSTR_flush), MP_ROM_PTR(&mp_stream_flush_obj) },
+    { MP_ROM_QSTR(MP_QSTR_txdone), MP_ROM_PTR(&machine_uart_txdone_obj) },
 
     { MP_ROM_QSTR(MP_QSTR_UART1), MP_ROM_INT(1) },
     { MP_ROM_QSTR(MP_QSTR_UART2), MP_ROM_INT(2) },
     { MP_ROM_QSTR(MP_QSTR_UART3), MP_ROM_INT(3) },
     { MP_ROM_QSTR(MP_QSTR_UART4), MP_ROM_INT(4) },
-    { MP_ROM_QSTR(MP_QSTR_FIVEBITS), MP_ROM_INT(5) },
-    { MP_ROM_QSTR(MP_QSTR_SIXBITS), MP_ROM_INT(6) },
-    { MP_ROM_QSTR(MP_QSTR_SEVENBITS), MP_ROM_INT(7) },
-    { MP_ROM_QSTR(MP_QSTR_EIGHTBITS), MP_ROM_INT(8) },
-    { MP_ROM_QSTR(MP_QSTR_STOPBITS_ONE), MP_ROM_INT(0) },
-    { MP_ROM_QSTR(MP_QSTR_STOPBITS_TWO), MP_ROM_INT(1) },
-    { MP_ROM_QSTR(MP_QSTR_PARITY_NONE), MP_ROM_INT(0) },
-    { MP_ROM_QSTR(MP_QSTR_PARITY_ODD), MP_ROM_INT(1) },
-    { MP_ROM_QSTR(MP_QSTR_PARITY_EVEN), MP_ROM_INT(3) },
+
+    { MP_ROM_QSTR(MP_QSTR_FIVEBITS), MP_ROM_INT(DATA_BITS_5) },
+    { MP_ROM_QSTR(MP_QSTR_SIXBITS), MP_ROM_INT(DATA_BITS_6) },
+    { MP_ROM_QSTR(MP_QSTR_SEVENBITS), MP_ROM_INT(DATA_BITS_7) },
+    { MP_ROM_QSTR(MP_QSTR_EIGHTBITS), MP_ROM_INT(DATA_BITS_8) },
+    { MP_ROM_QSTR(MP_QSTR_NINEBITS), MP_ROM_INT(DATA_BITS_9) },
+
+    { MP_ROM_QSTR(MP_QSTR_STOPBITS_ONE), MP_ROM_INT(STOP_BITS_1) },
+    { MP_ROM_QSTR(MP_QSTR_STOPBITS_ONE_P_FIVE), MP_ROM_INT(STOP_BITS_1) },
+    { MP_ROM_QSTR(MP_QSTR_STOPBITS_TWO), MP_ROM_INT(STOP_BITS_2) },
+
+    { MP_ROM_QSTR(MP_QSTR_PARITY_NONE), MP_ROM_INT(PARITY_NONE) },
+    { MP_ROM_QSTR(MP_QSTR_PARITY_ODD), MP_ROM_INT(PARITY_ODD) },
+    { MP_ROM_QSTR(MP_QSTR_PARITY_EVEN), MP_ROM_INT(PARITY_EVEN) },
 };
 STATIC MP_DEFINE_CONST_DICT(machine_uart_locals_dict, machine_uart_locals_dict_table);
 
-STATIC const mp_stream_p_t machine_uart_stream_p = {
-    .read = machine_uart_read,
-    .write = machine_uart_write,
-    .ioctl = machine_uart_ioctl,
+STATIC mp_uint_t machine_uart_read(mp_obj_t self_in, void* buf, mp_uint_t size, int* errcode)
+{
+    machine_uart_obj_t* self = MP_OBJ_TO_PTR(self_in);
+
+    // If timeout is -1, treat as non-blocking read (timeout = 0)
+    mp_uint_t effective_timeout = (self->timeout == (mp_uint_t)-1) ? 0 : self->timeout;
+
+    size_t    read_bytes = 0;
+    mp_uint_t start      = mp_hal_ticks_ms();
+    while (read_bytes < size && (mp_hal_ticks_ms() - start < effective_timeout)) {
+        int r = drv_uart_read(self->inst, (uint8_t*)buf + read_bytes, size - read_bytes);
+        if (r <= 0) {
+            break;
+        }
+        read_bytes += r;
+
+        // If timeout == 0, only attempt once
+        if (effective_timeout == 0) {
+            break;
+        }
+    }
+
+    if (read_bytes == 0) {
+        *errcode = MP_EAGAIN;
+        return MP_STREAM_ERROR;
+    }
+
+    return read_bytes;
+}
+
+STATIC mp_uint_t machine_uart_write(mp_obj_t self_in, const void* buf, mp_uint_t size, int* errcode)
+{
+    machine_uart_obj_t* self = MP_OBJ_TO_PTR(self_in);
+
+    int w = drv_uart_write(self->inst, (uint8_t*)buf, size);
+    if (w < 0) {
+        *errcode = MP_EAGAIN;
+        return MP_STREAM_ERROR;
+    }
+
+    return w;
+}
+
+STATIC mp_uint_t machine_uart_ioctl(mp_obj_t self_in, mp_uint_t request, uintptr_t arg, int* errcode)
+{
+    machine_uart_obj_t* self = MP_OBJ_TO_PTR(self_in);
+
+    *errcode      = 0;
+    mp_uint_t ret = 0;
+
+    switch (request) {
+    case MP_STREAM_POLL: {
+        mp_uint_t flags = arg;
+        ret             = 0;
+        if ((flags & MP_STREAM_POLL_RD) && drv_uart_recv_available(self->inst) > 0) {
+            ret |= MP_STREAM_POLL_RD;
+        }
+        if ((flags & MP_STREAM_POLL_WR)) { // assume always writable
+            ret |= MP_STREAM_POLL_WR;
+        }
+        return ret;
+    }
+    case MP_STREAM_FLUSH:
+        // Not implemented
+        return 0;
+    default:
+        *errcode = MP_EINVAL;
+        return MP_STREAM_ERROR;
+    }
+}
+
+STATIC const mp_stream_p_t uart_stream_p = {
+    .read    = machine_uart_read,
+    .write   = machine_uart_write,
+    .ioctl   = machine_uart_ioctl,
+    .is_text = false,
 };
 
+/* clang-format off */
 MP_DEFINE_CONST_OBJ_TYPE(
     machine_uart_type,
     MP_QSTR_UART,
     MP_TYPE_FLAG_ITER_IS_STREAM,
     make_new, machine_uart_make_new,
     print, machine_uart_print,
-    protocol, &machine_uart_stream_p,
+    protocol, &uart_stream_p,
     locals_dict, &machine_uart_locals_dict
-    );
+);
+/* clang-format on */
