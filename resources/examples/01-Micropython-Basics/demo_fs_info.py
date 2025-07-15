4b5798d655f8e7721747833ff705ff808e52fad4
kendryte747 <kendryte747@gmail.com>
2025-07-15 17:14:06 +0800

    Update sample.

--- resources/examples/01-Micropython-Basics/demo_fs_info.py
+++ resources/examples/01-Micropython-Basics/demo_fs_info.py
@@ -1,32 +1,42 @@
 import os
 
-S_IFMT = 0o170000
+# File mode constants (MicroPython-style)
+S_IFMT  = 0o170000
 S_IFDIR = 0o040000
 S_IFREG = 0o100000
 
 def sizeof_fmt(num, suffix='B'):
-    for unit in ['', 'K', 'M', 'G', 'T', 'P', 'E', 'Z']:
+    for unit in ['', 'K', 'M', 'G', 'T']:
         if abs(num) < 1024.0:
             return "%3.1f%s%s" % (num, unit, suffix)
         num /= 1024.0
-    return "%.1f%s%s" % (num, 'Yi', suffix)
+    return "%.1f%s%s" % (num, 'P', suffix)
 
 root_files = os.listdir('/')
 fs_info_list = []
+
 for f in root_files:
     fs_path = '/' + f
-    fs_stat = os.stat(fs_path)
-    size = fs_stat[6]
-    f_type = fs_stat[0] & S_IFMT
-    if f_type == S_IFDIR:
-        f_type = "DIR"
-    elif f_type == S_IFREG:
-        f_type = "FILE"
-    else:
-        f_type = "OTHER"
-    info = "%s [%s] size=%s" % (
-        fs_path, f_type,
-        sizeof_fmt(size)
-    )
-    fs_info_list.append(info)
-    print(info)
+
+    try:
+        # Get file type
+        st_mode = os.stat(fs_path)[0]
+        f_type = st_mode & S_IFMT
+        if f_type == S_IFDIR:
+            f_type_str = "DIR"
+        elif f_type == S_IFREG:
+            f_type_str = "FILE"
+        else:
+            f_type_str = "OTHER"
+
+        # Get filesystem stats
+        fs_stat = os.statvfs(fs_path)
+        total_bytes = fs_stat[0] * fs_stat[2]  # f_bsize * f_blocks
+        info = "%s [%s] size=%s" % (
+            fs_path, f_type_str,
+            sizeof_fmt(total_bytes)
+        )
+        fs_info_list.append(info)
+        print(info)
+    except Exception as e:
+        print(f"⚠️ Error accessing {fs_path}: {e}")
